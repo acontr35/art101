f940a90c5822998c3f760eaeb7730f9c32a1c336
Adrian Contreras <acontr35@ucsc.edu>
2023-10-25 08:01:08 -0700

    update to lab 4 and 5

--- lab5/js/lab.js
+++ lab5/js/lab.js
@@ -1,16 +1,22 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+// index.js - Lab 5 Data Types and Variables
+// Author: Adrian Contreras
+// Date: Oct 24, 2023
 
 // Constants
+make = "toyota"
+model = "tacoma"
+color = "grey"
+year = "2001"
+ownIt = "true"
+age = 2023 - year;
 
 // Functions
 
 // this is an example function and this comment tells what it doees and what parameters are passed to it.
-function myFunction(param1, param2) {
+document.writeln("make: " + make + "<br>") 
   // some code here
   // return results;
-}
+
 
 function main() {
   console.log("Main function started.");
