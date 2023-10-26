59c0aa993120d2df0fa3cb93422e22f41ea890b3
Adrian Contreras <acontr35@ucsc.edu>
2023-10-26 09:01:43 -0700

    Finished lab 5

--- lab5/js/lab.js
+++ lab5/js/lab.js
@@ -13,7 +13,12 @@ age = 2023 - year;
 // Functions
 
 // this is an example function and this comment tells what it doees and what parameters are passed to it.
-document.writeln("make: " + make + "<br>") 
+document.writeln("Make: " + make + "<br>");
+document.writeln("Model: " + model + "<br>");
+document.writeln("Color: " + color + "<br>");
+document.writeln("Year: " + year + "<br>");
+document.writeln("Own it: " + ownIt + "<br>");
+document.writeln("Age: " + age + "<br>");
   // some code here
   // return results;
 
