23babad93a0d25ae2197bf5ae30f2aa261811a44
Adrian Contreras <acontr35@ucsc.edu>
2023-10-29 22:35:16 -0700

    lab 6 finished

--- lab6/js/lab.js
+++ lab6/js/lab.js
@@ -1,11 +1,31 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+// index.js - Lab 6: Arrays and Objects
+// Author: Adrian Contreras
+// Date: 10/29/2023
 
 // Constants
+var myTransport = [
+  "bus",
+  "car",
+  "bike",
+  "legs.",
+];
+
+var myMainRide = {
+  make: "Toyota",
+  model: "Tacoma",
+  color: "grey",
+  year: 2001,
+  age: function() {
+      return 2023 - this.year;
+      }
+};
 
 // Functions
 
+document.writeln("How I get around Santa Cruz: " + myTransport + "<br>");
+
+document.writeln("<br>" + "My Main Ride: <pre>", 
+    JSON.stringify(myMainRide, null, '\t'), "</pre>");
 // this is an example function and this comment tells what it doees and what parameters are passed to it.
 function myFunction(param1, param2) {
   // some code here
