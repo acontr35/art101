3760011dc9ce9a6983254c8ef4387ec7bbacaf0f
Adrian Contreras <acontr35@ucsc.edu>
2023-11-02 17:07:05 -0700

    lab 7 setup

---
+++ lab7/js/lab.js
@@ -0,0 +1,27 @@
+// index.js - Complete Lab 7: functions
+// Author: Adrian Contreras
+// Date: 11/2/2022
+
+// Constants
+
+// Functions
+
+function userName() {
+    var 
+}
+
+
+// this is an example function and this comment tells what it doees and what parameters are passed to it.
+function myFunction(param1, param2) {
+    // some code here
+    // return results;
+  }
+  
+  function main() {
+    console.log("Main function started.");
+    // the code that makes everything happen
+  }
+  
+  // let's get this party started
+  main();
+  
