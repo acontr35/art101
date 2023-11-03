b7cff4d5d702e03bb54da2c792de7b5931f0d4b7
Adrian Contreras <acontr35@ucsc.edu>
2023-11-02 20:23:38 -0700

    lab 7 finished

--- lab7/js/lab.js
+++ lab7/js/lab.js
@@ -6,10 +6,23 @@
 
 // Functions
 
-function userName() {
-    var 
+function sortUserName() {
+    var userName = window.prompt("Hey, what's your name? Something dumb I bet. ");
+    console.log("userName =", userName);
+    // split string to array
+    var nameArray = userName.split('');
+    console.log("nameArray =", nameArray);
+    //sort the Array
+    var nameArraySort = nameArray.sort();
+    console.log("nameArraySort =", nameArraySort);
+    //join array back to a string
+    var nameSorted = nameArraySort.join('');
+    console.log("nameSorted =", nameSorted);
+    return nameSorted;
 }
 
+document.writeln("Oh hey, so your name was gross, but I made it better.", 
+        sortUserName(), "</br>");
 
 // this is an example function and this comment tells what it doees and what parameters are passed to it.
 function myFunction(param1, param2) {
