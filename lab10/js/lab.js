a46f0adcb88fc5843cdec21ccf73bcbe3dc998a9
Adrian Contreras <acontr35@ucsc.edu>
2023-11-16 11:03:39 -0800

    creste lab 10

---
+++ lab10/js/lab.js
@@ -0,0 +1,9 @@
+// index.js - Complete Lab 10: Libraries and jQuery
+// Author: Adrian Contreras
+// Date: 11/16/2023
+
+// Constants
+
+
+// Functions
+// add a click listener to the challenge button
