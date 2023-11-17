67f1efe2c80890992130f1afbc54c804d2f151b3
Adrian Contreras <acontreras2552@gmail.com>
2023-11-16 21:50:46 -0800

    lab 10 trial

--- lab10/js/lab.js
+++ lab10/js/lab.js
@@ -1,9 +1,24 @@
-// index.js - Complete Lab 10: Libraries and jQuery
+// index.js - Complete Lab 10: Libraries and jQuery  - creating a button that lets the user have a fake text conversation.
 // Author: Adrian Contreras
 // Date: 11/16/2023
 
 // Constants
+//click listener for button
+$("make-convo").click(function(){
+    const newText = generateRandomText();
+});
 
+$("#output").append('<div class="text"><p>' + newText + '</p></div>');
 
 // Functions
+function generateRandomText() {
+    const text ="me when I when you when me when like when you and then you when I but also how you then when you if you ";
+const min = 3;
+const max = 100;
+const randLen = Math.floor(Math.random() * (max - min +1)) + min;
+//get a random starting index to slice the random text
+const randStart = Math.floor(Math.random() * (text.length - randLen + 1));
+// generate the random text
+return text.slice(randStart, randStart + randLen);
+}
 // add a click listener to the challenge button
