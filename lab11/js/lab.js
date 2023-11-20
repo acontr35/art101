1f4a421500b3ca2455311456aa8792cfd1f7873e
Adrian Contreras <acontr35@ucsc.edu>
2023-11-20 00:26:28 -0800

    lab 11 done and some links

--- lab11/js/lab.js
+++ lab11/js/lab.js
@@ -1,27 +1,27 @@
-// index.js - Complete Lab 10: Libraries and jQuery  - creating a button that lets the user have a fake text conversation.
+// index.js - Complete Lab 11: JavaScript Events and Forms
 // Author: Adrian Contreras
-// Date: 11/16/2023
+// Date: 11/19/2023
 
 // Constants
-//click listener for button
+
+// Sorts the characters of a string in alphabetical order.
 
 
 
 // Functions
-function generateRandomText() {
-const text ="me when I when you when me when like when you and then you when I but also how you then when you if you ";
-const min = 3;
-const max = 100;
-const randLen = Math.floor(Math.random() * (max - min +1)) + min;
-//get a random starting index to slice the random text
-const randStart = Math.floor(Math.random() * (text.length - randLen + 1));
-// generate the random text
-return text.slice(randStart, randStart + randLen);
-}
-// add a click listener to the challenge button
-$("#make-convo").click(function() { 
-const newText = generateRandomText();
-$("#output").append('<div class="text"><p>' + newText + '</p></div>');
-});
 
+function sortString(inputString) {
+    // We have to convert our string to an array and back again to sort it
+    return inputString.split('').sort().join('');
+  }
+  
+// click listener for button
+$("#submit").click(function(){
+// get value of input field
+const userName = $("#user-name").val();
+// now let's sort it
+userNameSorted = sortString(userName);
+// append a new div to our output div
+$("#output").html('<div class="text"><p>' + userNameSorted + '</p></div>');
+});
 
