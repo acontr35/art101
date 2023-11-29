61bd676b4c65a3bdb5d85935245e5ec5b3b5e572
Adrian Contreras <acontr35@ucsc.edu>
2023-11-29 10:52:45 -0800

    working on lab 14

---
+++ lab14/js/lab.js
@@ -0,0 +1,45 @@
+// index.js - Complete Lab 13: Loops
+// Author: Adrian Contreras
+// Date: 11/26/2023
+
+// Constants
+
+
+
+
+
+// Functions
+
+
+
+
+for (var index=1;index<=200;index++){
+    var number = '';
+
+    if (index%15 == 0) {
+        $("#output").append("<p>fizz-buzz</p>")
+    }
+
+    else if (index%21 == 0) {
+        $("#output").append("<p>fizz-boom</p>")
+    }
+    else if (index%40 == 0) {
+        $("#output").append("<p>buzz-fizz</p>")
+    }
+    else if (index%3 == 0) {
+        $("#output").append("<p>fizz</p>")
+    }
+    else if (index%5 == 0) {
+        $("#output").append("<p>buzz</p>")
+    }
+    else if (index%7 == 0) {
+        $("#output").append("<p>boom</p>")
+    }
+    else {
+        number = [...String(index)].join(''); //angela used wesbot 
+        $("#output").append(`<p>${number}</p>`); //same
+    }
+     
+        
+}
+
