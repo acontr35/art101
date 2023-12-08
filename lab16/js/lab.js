5773f079348fcecfbb16cdb6a9898a83d22eac29
Adrian Contreras <acontr35@ucsc.edu>
2023-12-07 17:24:13 -0800

    lab 16 setup

---
+++ lab16/js/lab.js
@@ -0,0 +1,35 @@
+// index.js - Complete Lab 15: Ajax
+// Author: Adrian Contreras
+// Date: 12/4/2023
+
+
+     $.ajax({
+        // The URL for the request (from the api docs)
+        url: "https://xkcd.com/info.0.json",
+        // The data to send (will be converted to a query string)
+    
+        // Whether this is a POST or GET request
+        type: "GET",
+        // The type of data we expect back
+        dataType : "json",
+        // What do we do when the api call is successful
+        //   all the action goes in here
+        success: function(data) {
+            // do stuff
+            console.log(data);
+            var printableData = "<pre>" + JSON.stringify(data) + "</pre>";
+            console.log(printableData);
+            $("#output").append("<p>" + data.month + comicObj.title);
+            $("#output").append("<p>" + data.month + data.day);
+            $("#output").append("<p>" + data.safe_title);
+            $("#output").append("<p>" + data.transcript);
+            $("#output").append("<p>" + data.alt);
+            
+          
+        },
+        // What we do if the api call fails
+        error: function (jqXHR, textStatus, errorThrown) { 
+            // do stuff
+            console.log("Error:", textStatus, errorThrown);
+        }
+    });
--- labfinal/js/lab.js
+++ labfinal/js/lab.js
@@ -1,37 +1,5 @@
-// index.js - Complete Lab 15: Ajax
+// index.js - Complete Lab 16: JSON and APIs
 // Author: Adrian Contreras
-// Date: 12/4/2023
+// Date: 12/7/2023
 
 
-var myButton = document.getElementById("submit");
-myButton.addEventListener("click", function() {
-    var number = document.getElementById("input").value;
-    console.log(number)
-    URL = "https://pokeapi.co/api/v2/pokemon/" + number + "/"
-
-     $.ajax({
-        // The URL for the request (from the api docs)
-        url: URL,
-        // The data to send (will be converted to a query string)
-    
-        // Whether this is a POST or GET request
-        type: "GET",
-        // The type of data we expect back
-        dataType : "json",
-        // What do we do when the api call is successful
-        //   all the action goes in here
-        success: function(data) {
-            // do stuff
-            console.log(data);
-            var printableData = "<pre>" + JSON.stringify(data) + "</pre>";
-            console.log(printableData);
-            $("#output1").append("<p>" + data.name);
-            $("#output1").append( "Base Experience: ", + data.base_experience);
-        },
-        // What we do if the api call fails
-        error: function (jqXHR, textStatus, errorThrown) { 
-            // do stuff
-            console.log("Error:", textStatus, errorThrown);
-        }
-    })
-})
