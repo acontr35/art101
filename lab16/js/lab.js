f0d9391efca7ebd87b5b605719cda51e97a544ce
Adrian Contreras <acontr35@ucsc.edu>
2023-12-08 01:39:13 -0800

    lab 16 finished

--- lab16/js/lab.js
+++ lab16/js/lab.js
@@ -1,36 +1,51 @@
-// index.js - Complete Lab 15: Ajax
+// index.js - Complete Lab 16: JSON and API's
 // Author: Adrian Contreras
 // Date: 12/4/2023
+ 
+var number = 2000
 
 
-     $.ajax({
-        // The URL for the request (from the api docs)
-        url: "https://xkcd.com/info.0.json",
-        // The data to send (will be converted to a query string)
+// Using the core $.ajax() method
+$.ajax({
+    // The URL for the request (from the api docs)
+
+   url: "https://xkcd.com/" + number + "/info.0.json",
+  
+    // Whether this is a POST or GET request
+    type: "GET",
+    // The type of data we expect back
+    dataType : "json",
+    // What do we do when the api call is successful
+    //   all the action goes in here
+    success: function(data) {
+        // do stuff
+
+        console.log(data);
+        var comicObj = data;
+        var imageUrl = data.img;
+        var printableData = "<pre>" + JSON.stringify(comicObj) + "</pre>";
+        console.log(printableData);
+        $("#output").append("<h1>" + comicObj.title + "<h1>");
+        var html = `<div id="imageblock">
+        <img src="${imageUrl}">
+      </div>`
+
+    // console.log("My new html: \n", html);
+    $("#output").html(html);
+        $("#output").append("<h1>" + comicObj.safe_title)
+        $("#output").append("<p>" + comicObj.alt);
+    },
     
-        // Whether this is a POST or GET request
-        type: "GET",
-        // The type of data we expect back
-        dataType : "json",
-        // What do we do when the api call is successful
-        //   all the action goes in here
-        success: function(data) {
-            // do stuff
-            console.log(data);
-            var comicObj = data;
-            var printableData = "<pre>" + JSON.stringify(data) + "</pre>";
-            console.log(printableData);
-            $("#output").append("<p>" + comicObj.month + comicObj.title);
-            $("#output").append("<p>" + comicObj.month + comicObj.day);
-            $("#output").append("<p>" + comicObj.safe_title);
-            $("#output").append("<p>" + comicObj.transcript);
-            $("#output").append("<p>" + comicObj.alt);
-            
-          
-        },
-        // What we do if the api call fails
-        error: function (jqXHR, textStatus, errorThrown) { 
-            // do stuff
-            console.log("Error:", textStatus, errorThrown);
-        }
-    });
+    // What we do if the api call fails
+    error: function (jqXHR, textStatus, errorThrown) { 
+        // do stuff
+        console.log("Error:", textStatus, errorThrown);
+    }
+  });
+
+
+  var myButton = document.getElementById("nextbutton")
+  myButton.addEventListener("click",  function chamgeUrl() {
+    comicObj.num + 1
+    console.log("add 1")
+  })
